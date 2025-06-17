a3348bae62cbe9284e89df7ca3089f18a5dcbace
yejiniee <02doyoung01@naver.com>
2025-06-17 13:18:49 +0900

    🐛 fix: 검색 시 전체 데이터를 대상으로 하도록 API 및 커스텀 훅 수정

--- src/features/reservations/api/getReservations.js
+++ src/features/reservations/api/getReservations.js
@@ -1,6 +1,11 @@
 import supabase from "@/supabase";
 
-export default async function getReservations({ status, page = 1, limit = 6 }) {
+export default async function getReservations({
+  status,
+  page = 1,
+  limit = 6,
+  keyword,
+}) {
   const from = (page - 1) * limit;
   const to = from + limit - 1;
 
@@ -10,10 +15,22 @@ export default async function getReservations({ status, page = 1, limit = 6 }) {
     .order("created_at", { ascending: false })
     .range(from, to);
 
-  if (status === "confirmed") query = query.eq("status", "confirmed");
-  else if (status === "unconfirmed") query = query.eq("status", "unconfirmed");
-  else {
+  // status 필터
+  if (status === "confirmed") {
+    query = query.eq("status", "confirmed");
+  } else if (status === "unconfirmed") {
+    query = query.eq("status", "unconfirmed");
+  } else {
     query = query.or("status.eq.confirmed,status.eq.unconfirmed");
+  }
+
+  // keyword 필터
+  if (keyword && keyword.trim() !== "") {
+    const search = `%${keyword.trim()}%`;
+
+    query = query.or(
+      `name.ilike.${search},email.ilike.${search},whatsapp.ilike.${search},reservation_number.ilike.${search}`,
+    );
   }
 
   const { data, error, count } = await query;
--- src/features/reservations/hooks/useGetReservations.js
+++ src/features/reservations/hooks/useGetReservations.js
@@ -4,11 +4,18 @@ import getReservations from "../api/getReservations";
 /**
  * @param {"confirmed" | "unconfirmed" | undefined} selectedTabLabel
  */
-export default function useGetReservations({ status, page = 1, limit }) {
+export default function useGetReservations({
+  status,
+  page = 1,
+  limit,
+  keyword,
+  // onError,
+}) {
   return useQuery({
-    queryKey: ["reservations", status, page],
-    queryFn: () => getReservations({ status, page, limit }),
+    queryKey: ["reservations", status, page, keyword],
+    queryFn: () => getReservations({ status, page, limit, keyword }),
     keepPreviousData: true,
     staleTime: 1000 * 30,
+    // onError,
   });
 }
