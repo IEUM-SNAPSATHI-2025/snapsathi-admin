6b57725ae689744a286c3e3c8dcfdbfde299d051
yejiniee <02doyoung01@naver.com>
2025-06-17 13:34:41 +0900

    🐛 fix: 문의관리 페이지에도 검색 시 전체 데이터를 대상으로 하도록 API 및 커스텀 훅 수정

--- src/features/reservations/hooks/useGetInquiries.js
+++ src/features/reservations/hooks/useGetInquiries.js
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import getInquiries from "../api/getInquiries";
 
-export default function useGetInquiries({ page = 1, limit }) {
+export default function useGetInquiries({ page = 1, limit, keyword }) {
   return useQuery({
-    queryKey: ["inquiries", page],
-    queryFn: () => getInquiries({ page, limit }),
+    queryKey: ["inquiries", page, keyword],
+    queryFn: () => getInquiries({ page, limit, keyword }),
     keepPreviousData: true,
     staleTime: 1000 * 30,
   });
